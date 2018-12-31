fb06241205a234528e15f19ff2defb2afc592b33
zedenet <33369806+zedenet@users.noreply.github.com>
2018-12-31 03:49:04 -0800

    Add files via upload
    a simple java program which accept two integer numbers from the user and display the sum of the two.

---
+++ Addition.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Addition{
+public static void main(String args[]){
+int a,b,c;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the first num");
+a=sc.nextInt();
+System.out.println("Enter the second num");
+b=sc.nextInt();
+c=a+b;
+System.out.println("sum="+c);
+}
+}
